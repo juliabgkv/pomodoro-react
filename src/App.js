cae8f5743746489f27d696c49ce31b0d8f097258
yuliabgkv <bugakovayk@gmail.com>
2024-05-29 00:01:00 +0300

    style: add styles

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import { useEffect, useState } from 'react';
 import { msToTime, timeStringify } from './utils/timeConverter';
-import './App.css';
 import ModeControls from './components/ModeControls';
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
 
-const T = 2000;
-
-// const INITIAL_TIMERS = {
-//   pomodori: 1500000,
-//   shortBreak: 180000,
-//   longBreak: 900000
-// };
+const INITIAL_TIMERS = {
+  pomodori: 1500000,
+  shortBreak: 180000,
+  longBreak: 900000
+};
 
 // for quick test functionality (remove later)
-const INITIAL_TIMERS = {
-  pomodori: 3000,
-  shortBreak: 2000,
-  longBreak: 1000
-};
+// const INITIAL_TIMERS = {
+//   pomodori: 3000,
+//   shortBreak: 2000,
+//   longBreak: 1000
+// };
 
 function App() {
   const [timers, setTimers] = useState(INITIAL_TIMERS);
@@ -82,38 +80,70 @@ function App() {
     });
   }
 
+  const progress = (currentTimerState.timeLeft/timers[mode])*100;
   let content;
+  let btnTitle = 'Start';
+
+  if (currentTimerState.isActive) btnTitle = 'Pause';
+  else if (currentTimerState.timeLeft < timers[mode] && !currentTimerState.isActive) btnTitle = 'Resume';
 
   if (currentTimerState.timeLeft > 0) {
     const time = msToTime(currentTimerState.timeLeft);
     const timeStr = timeStringify(time);
-  
-    let btnText = 'Start';
-    if (currentTimerState.isActive) btnText = 'Pause';
-    else if (currentTimerState.timeLeft < timers[mode] && !currentTimerState.isActive) btnText = 'Resume';
 
-    content = <div>
-                <div>{timeStr}</div>
-                  <button onClick={handleControlTimer}>
-                    {btnText}
-                  </button>
-              </div>;
+    content = <>
+                <div className='time'>{timeStr}</div>
+              </>;
   } else if (currentTimerState.timeLeft === 0 && (mode === 'shortBreak' || mode === 'longBreak')) {
     content = <p>Time to back to work!</p>;
   } else if (currentTimerState.timeLeft === 0 && mode === 'pomodori') {
     content = <p>Take a break!</p>
   }
 
+  let resetBtnClasses = 'reset-timer-btn';
+
+  if (currentTimerState.timeLeft < timers[mode]) resetBtnClasses += ' active';
+
   return (
-    <>
+    <div className="container">
       <ModeControls
         modes={Object.keys(timers)}
         currentMode={mode}
         onModeChange={handleModeChange}
       />
-      {content}
-      <button onClick={handleResetTimer}>Reset</button>
-    </>
+      <div className="progress-bar-container">
+        <CircularProgressbarWithChildren
+          value={progress}
+          strokeWidth={3}
+          styles={buildStyles({
+            strokeLinecap: 'round',
+            pathTransitionDuration: 0.5,
+            pathColor: `rgba(250, 249, 246)`,
+            // textColor: '#f88',
+            trailColor: '#8B0000',
+            // backgroundColor: '#3e98c7',
+          })}
+        >
+        {content}
+        </CircularProgressbarWithChildren>
+      </div>
+      <div className='control-buttons'>
+        {currentTimerState.timeLeft !== 0 &&
+          <button
+            title={btnTitle}
+            onClick={handleControlTimer}
+            className={'control-timer-btn ' + (currentTimerState.isActive ? 'pause' : 'start')}
+          >
+          </button>
+        }
+        <button
+          title='Reset'
+          onClick={handleResetTimer}
+          className={resetBtnClasses}
+        >
+        </button>
+      </div>
+    </div>
   );
 }
 
--- src/components/ModeControls.jsx
+++ src/components/ModeControls.jsx
@@ -6,21 +6,24 @@ function ModeControls({ modes, currentMode, onModeChange }) {
     }
 
     return (
-        <form>
+        <form className="mode-controls-form">
             {modes.map(mode => {
                 const label = idToLabel(mode);
 
                 return (
-                    <div key={mode}>
-                        <input
-                            type="radio"
-                            id={mode}
-                            value={mode}
-                            checked={mode === currentMode}
-                            onChange={handleOnChange}
-                        ></input>
-                        <label htmlFor={mode}>{label}</label>
-                    </div>
+                    // <div key={mode} className="mode-control">
+                        
+                        <label htmlFor={mode} key={mode} className="mode-control">
+                            <input
+                                type="radio"
+                                id={mode}
+                                value={mode}
+                                checked={mode === currentMode}
+                                onChange={handleOnChange}
+                            ></input>
+                            {label}
+                        </label>
+                    // </div>
                 );
             })}
         </form>
